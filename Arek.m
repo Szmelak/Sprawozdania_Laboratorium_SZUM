431f26a7c8f1b92f71f8fbf1fcec0b239ad96ff2
Szmelak <34351724+Szmelak@users.noreply.github.com>
2018-11-13 12:40:39 +0100

    Update Arek.m

--- Arek.m
+++ Arek.m
@@ -1,3 +1,7 @@
+% Sprawozdanie.1 Arkadiusz Szmelcuch 36780 AiR
+% Po stworzeniu symulacji w terminalach, otrzymujemy ,,topic''. Przechowywane w nim są informacje o położeniu żółwia. 
+% Następnie w programie Matlab pobieramy informacje z ,,topic" i tworzymy wizualizację. Poniżej kod programu.
+
 while(1)
     zolw = rossubscriber('/turtle1/pose');
     dane = receive(zolw);
